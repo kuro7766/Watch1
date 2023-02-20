6a81d650f415d77ca2a76f7f1727767ad6c43458
kuro <7445841+kuro7766@user.noreply.gitee.com>
2023-02-20 18:42:56 +0800

    add boot receiver

---
+++ app/src/main/java/com/luoli/lockandlightscreendemo/Receiver/BootReceiver.java
@@ -0,0 +1,24 @@
+package com.luoli.lockandlightscreendemo.Receiver;
+
+import android.app.admin.DeviceAdminReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+
+public class BootReceiver extends BroadcastReceiver
+{
+    public BootReceiver()
+    {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+        if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED"))
+        {
+            Intent i = new Intent(context, MainActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+        }
+    }
+}
